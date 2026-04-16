fa19b5dcfb56432ed4cbbf5c49c44336dbce7465
vankayalajaideep130070-ops <vankayala.jaideep130070@marwadiuniversity.ac.in>
2026-04-16 11:42:54 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+require("dotenv").config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("MongoDB Connected"))
+.catch(err=>console.log(err))
+
+app.use("/api/alerts", require("./routes/alerts"))
+app.use("/api/auth", require("./routes/auth"))
+
+app.listen(5000, ()=>console.log("Server running on port 5000"))
